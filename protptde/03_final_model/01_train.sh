3bbdc363e2fe289712e6e0178ce60b5233da7fc1
Li Zheyu <67777262+lizheyu0722@users.noreply.github.com>
2025-12-23 10:13:52 +0800

    Update 01_train.sh

--- protptde/03_final_model/01_train.sh
+++ protptde/03_final_model/01_train.sh
@@ -8,4 +8,3 @@ nohup bash 2000.sh > log/2000.log 2>&1 &
 nohup bash 2001.sh > log/2001.log 2>&1 &
 nohup bash 2002.sh > log/2002.log 2>&1 &
 nohup bash 2003.sh > log/2003.log 2>&1 &
-nohup bash 2004.sh > log/2004.log 2>&1 &
